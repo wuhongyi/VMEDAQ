118c73afdcd4efef8091cf4ace6fab2c09318bac
wuhongyi <wuhongyi@qq.com>
2017-08-16 15:37:19 +0800

    20170816

--- source/setup.sh
+++ source/setup.sh
@@ -7,6 +7,10 @@ cd CAENVMELib-2.50/lib
 sh install_x64
 cd ../../
 
+tar -xvzf CAENComm-1.2-build20140211.tgz
+cd CAENComm-1.2/lib
+sh install_x64
+cd ../../
 
 tar -xvzf CAENUpgrader-1.6.0-build20161104.tgz
 cd CAENUpgrader-1.6.0
@@ -16,7 +20,7 @@ make install
 cd ..
 
 
-rm -rf CAENVMELib-2.50  CAENUpgrader-1.6.0 
+rm -rf CAENVMELib-2.50  CAENComm-1.2 CAENUpgrader-1.6.0 
 echo "Install all software done !"
 
 
