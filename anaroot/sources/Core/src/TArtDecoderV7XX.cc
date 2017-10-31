cdf388d4262bb18a90b4750bd67628d7b511650b
wuhongyi <wuhongyi@pku.edu.cn>
2017-10-31 23:34:48 +0800

    20171031

--- anaroot/sources/Core/src/TArtDecoderV7XX.cc
+++ anaroot/sources/Core/src/TArtDecoderV7XX.cc
@@ -36,17 +36,17 @@ int TArtDecoderV7XX::Decode(unsigned char* &buf, const unsigned int& size,
    int evtflag = 0;
 
    for (int i=0; i<evtsize; i++) {
-      ih = (evtdata[i]&0x06000000);
-      if (ih == 0x02000000) {
+     ih = (evtdata[i]&0x06000000);//bit25 bit26
+      if (ih == 0x02000000) { //bit25==1  the Header
          igeo = (evtdata[i]&0xf8000000)>>27;
          evtflag = 1;
-      } else if (ih == 0 && evtflag == 1) {
+      } else if (ih == 0 && evtflag == 1) {//bit25==0 and bit26==0 the data word format
 	//ich = (evtdata[i]&0x003f0000) >> 16;
 	ich = (evtdata[i]&0x001f0000) >> 16;  // 2014.05.02 H.B.
-	 TArtRawDataObject *rdata = new TArtRawDataObject(igeo,ich,evtdata[i]&0x1fff);
+	TArtRawDataObject *rdata = new TArtRawDataObject(igeo,ich,evtdata[i]&0x1fff);//bit12-0
 	 rawseg->PutData(rdata);
 
-      } else if (ih == 0x04000000) {
+      } else if (ih == 0x04000000) {//bit26==1 the End Of Block
          evtflag = 0;
 	 TArtRawDataObject *rdata = new TArtRawDataObject(igeo,evtdata[i]&0xffffff);
 	 rawseg->PutData(rdata);
--- checkcnt/main.cc
+++ checkcnt/main.cc
@@ -4,9 +4,9 @@
 // Author: Hongyi Wu(吴鸿毅)
 // Email: wuhongyi@qq.com 
 // Created: 五 10月 13 21:05:34 2017 (+0800)
-// Last-Updated: 三 10月 25 16:39:53 2017 (+0800)
+// Last-Updated: 四 10月 26 20:02:45 2017 (+0800)
 //           By: Hongyi Wu(吴鸿毅)
-//     Update #: 15
+//     Update #: 16
 // URL: http://wuhongyi.cn 
 
 #include "UserDefine.hh"
@@ -68,31 +68,39 @@ int main(int argc, char *argv[])
   TString printpdfopenflag = "[";
   TString printpdfcloseflag = "]";
   char tempchar[128];
+
+  //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
   
-#ifdef v785_checkcnt //ok
   printpdf = pdffilename + printpdfopenflag;
   c1->Print(printpdf.Data());//No actual print, just open
+
+#ifdef v785_checkcnt //ok
 #if v785num > 1
-
-  for (int i = 1; i < v785num; ++i)
+  for (int i = 0; i < v785num; ++i)
     {
-      sprintf(tempchar,"acnt[0]-acnt[%d]",i);
-      t->Draw(tempchar);
-      c1->Update();
-      c1->Print(pdffilename);
-    }
-
-#ifdef v792_checkcnt //ok
-#if v792num > 1
-  for (int i = 0; i < v792num; ++i)
-    {
-      sprintf(tempchar,"acnt[0]-qcnt[%d]",i);
+      sprintf(tempchar,"nevt-acnt[%d]",i);
       t->Draw(tempchar);
       c1->Update();
       c1->Print(pdffilename);
     }
 #else
-  t->Draw("acnt[0]-qcnt");
+  t->Draw("nevt-acnt");
+  c1->Update();
+  c1->Print(pdffilename);
+#endif
+#endif  
+
+#ifdef v792_checkcnt //ok
+#if v792num > 1
+  for (int i = 0; i < v792num; ++i)
+    {
+      sprintf(tempchar,"nevt-qcnt[%d]",i);
+      t->Draw(tempchar);
+      c1->Update();
+      c1->Print(pdffilename);
+    }
+#else
+  t->Draw("nevt-qcnt");
   c1->Update();
   c1->Print(pdffilename);
 #endif
@@ -102,29 +110,29 @@ int main(int argc, char *argv[])
 #if v775num > 1
   for (int i = 0; i < v775num; ++i)
     {
-      sprintf(tempchar,"acnt[0]-tcnt[%d]",i);
+      sprintf(tempchar,"nevt-tcnt[%d]",i);
       t->Draw(tempchar);
       c1->Update();
       c1->Print(pdffilename);
     }
 #else
-  t->Draw("acnt[0]-tcnt");
+  t->Draw("nevt-tcnt");
   c1->Update();
   c1->Print(pdffilename);
 #endif  
-#endif
+#endif  
 
 #ifdef madc32_checkcnt //ok
 #if madc32num > 1
   for (int i = 0; i < madc32num; ++i)
     {
-      sprintf(tempchar,"acnt[0]-mcnt[%d]",i);
+      sprintf(tempchar,"nevt-mcnt[%d]",i);
       t->Draw(tempchar);
       c1->Update();
       c1->Print(pdffilename);
     }
 #else
-  t->Draw("acnt[0]-mcnt");
+  t->Draw("nevt-mcnt");
   c1->Update();
   c1->Print(pdffilename);
 #endif    
@@ -134,93 +142,174 @@ int main(int argc, char *argv[])
 #if v1x90num > 1
   for (int i = 0; i < v1x90num; ++i)
     {
-      sprintf(tempchar,"acnt[0]-gcnt[%d]",i);
+      sprintf(tempchar,"nevt-gcnt[%d]",i);
       t->Draw(tempchar);
       c1->Update();
       c1->Print(pdffilename);
     }
 #else
-  t->Draw("acnt[0]-gcnt");
+  t->Draw("nevt-gcnt");
   c1->Update();
   c1->Print(pdffilename);
 #endif  
 #endif
 
-#else //v785==1
-
-#ifdef v792_checkcnt //ok
-#if v792num > 1
-  for (int i = 0; i < v792num; ++i)
-    {
-      sprintf(tempchar,"acnt-qcnt[%d]",i);
-      t->Draw(tempchar);
-      c1->Update();
-      c1->Print(pdffilename);
-    }
-#else
-  t->Draw("acnt-qcnt");
-  c1->Update();
-  c1->Print(pdffilename);
-#endif
-#endif
-
-#ifdef v775_checkcnt //ok
-#if v775num > 1
-  for (int i = 0; i < v775num; ++i)
-    {
-      sprintf(tempchar,"acnt-tcnt[%d]",i);
-      t->Draw(tempchar);
-      c1->Update();
-      c1->Print(pdffilename);
-    }
-#else
-  t->Draw("acnt-tcnt");
-  c1->Update();
-  c1->Print(pdffilename);
-#endif  
-#endif
-
-#ifdef madc32_checkcnt //ok
-#if madc32num > 1
-  for (int i = 0; i < madc32num; ++i)
-    {
-      sprintf(tempchar,"acnt-mcnt[%d]",i);
-      t->Draw(tempchar);
-      c1->Update();
-      c1->Print(pdffilename);
-    }
-#else
-  t->Draw("acnt-mcnt");
-  c1->Update();
-  c1->Print(pdffilename);
-#endif    
-#endif
-
-#ifdef v1x90_checkcnt //ok
-#if v1x90num > 1
-  for (int i = 0; i < v1x90num; ++i)
-    {
-      sprintf(tempchar,"acnt-gcnt[%d]",i);
-      t->Draw(tempchar);
-      c1->Update();
-      c1->Print(pdffilename);
-    }
-#else
-  t->Draw("acnt-gcnt");
-  c1->Update();
-  c1->Print(pdffilename);
-#endif  
-#endif
   
-#endif//v785>=1
   printpdf = pdffilename + printpdfcloseflag;
   c1->Print(printpdf.Data());//No actual print, just close.
-#else//v785 not define
-  cout<<"V785 插件没有定义，请联系吴鸿毅(wuhongyi@qq.com)升级本程序！！！"<<endl;
-#endif
-
   
+  //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+  
+  
+// #ifdef v785_checkcnt //ok
+//   printpdf = pdffilename + printpdfopenflag;
+//   c1->Print(printpdf.Data());//No actual print, just open
+// #if v785num > 1
 
+//   for (int i = 1; i < v785num; ++i)
+//     {
+//       sprintf(tempchar,"acnt[0]-acnt[%d]",i);
+//       t->Draw(tempchar);
+//       c1->Update();
+//       c1->Print(pdffilename);
+//     }
+
+// #ifdef v792_checkcnt //ok
+// #if v792num > 1
+//   for (int i = 0; i < v792num; ++i)
+//     {
+//       sprintf(tempchar,"acnt[0]-qcnt[%d]",i);
+//       t->Draw(tempchar);
+//       c1->Update();
+//       c1->Print(pdffilename);
+//     }
+// #else
+//   t->Draw("acnt[0]-qcnt");
+//   c1->Update();
+//   c1->Print(pdffilename);
+// #endif
+// #endif
+
+// #ifdef v775_checkcnt //ok
+// #if v775num > 1
+//   for (int i = 0; i < v775num; ++i)
+//     {
+//       sprintf(tempchar,"acnt[0]-tcnt[%d]",i);
+//       t->Draw(tempchar);
+//       c1->Update();
+//       c1->Print(pdffilename);
+//     }
+// #else
+//   t->Draw("acnt[0]-tcnt");
+//   c1->Update();
+//   c1->Print(pdffilename);
+// #endif  
+// #endif
+
+// #ifdef madc32_checkcnt //ok
+// #if madc32num > 1
+//   for (int i = 0; i < madc32num; ++i)
+//     {
+//       sprintf(tempchar,"acnt[0]-mcnt[%d]",i);
+//       t->Draw(tempchar);
+//       c1->Update();
+//       c1->Print(pdffilename);
+//     }
+// #else
+//   t->Draw("acnt[0]-mcnt");
+//   c1->Update();
+//   c1->Print(pdffilename);
+// #endif    
+// #endif
+
+// #ifdef v1x90_checkcnt //ok
+// #if v1x90num > 1
+//   for (int i = 0; i < v1x90num; ++i)
+//     {
+//       sprintf(tempchar,"acnt[0]-gcnt[%d]",i);
+//       t->Draw(tempchar);
+//       c1->Update();
+//       c1->Print(pdffilename);
+//     }
+// #else
+//   t->Draw("acnt[0]-gcnt");
+//   c1->Update();
+//   c1->Print(pdffilename);
+// #endif  
+// #endif
+
+// #else //v785==1
+
+// #ifdef v792_checkcnt //ok
+// #if v792num > 1
+//   for (int i = 0; i < v792num; ++i)
+//     {
+//       sprintf(tempchar,"acnt-qcnt[%d]",i);
+//       t->Draw(tempchar);
+//       c1->Update();
+//       c1->Print(pdffilename);
+//     }
+// #else
+//   t->Draw("acnt-qcnt");
+//   c1->Update();
+//   c1->Print(pdffilename);
+// #endif
+// #endif
+
+// #ifdef v775_checkcnt //ok
+// #if v775num > 1
+//   for (int i = 0; i < v775num; ++i)
+//     {
+//       sprintf(tempchar,"acnt-tcnt[%d]",i);
+//       t->Draw(tempchar);
+//       c1->Update();
+//       c1->Print(pdffilename);
+//     }
+// #else
+//   t->Draw("acnt-tcnt");
+//   c1->Update();
+//   c1->Print(pdffilename);
+// #endif  
+// #endif
+
+// #ifdef madc32_checkcnt //ok
+// #if madc32num > 1
+//   for (int i = 0; i < madc32num; ++i)
+//     {
+//       sprintf(tempchar,"acnt-mcnt[%d]",i);
+//       t->Draw(tempchar);
+//       c1->Update();
+//       c1->Print(pdffilename);
+//     }
+// #else
+//   t->Draw("acnt-mcnt");
+//   c1->Update();
+//   c1->Print(pdffilename);
+// #endif    
+// #endif
+
+// #ifdef v1x90_checkcnt //ok
+// #if v1x90num > 1
+//   for (int i = 0; i < v1x90num; ++i)
+//     {
+//       sprintf(tempchar,"acnt-gcnt[%d]",i);
+//       t->Draw(tempchar);
+//       c1->Update();
+//       c1->Print(pdffilename);
+//     }
+// #else
+//   t->Draw("acnt-gcnt");
+//   c1->Update();
+//   c1->Print(pdffilename);
+// #endif  
+// #endif
+  
+// #endif//v785>=1
+//   printpdf = pdffilename + printpdfcloseflag;
+//   c1->Print(printpdf.Data());//No actual print, just close.
+// #else//v785 not define
+//   cout<<"V785 插件没有定义，请联系吴鸿毅(wuhongyi@qq.com)升级本程序！！！"<<endl;
+// #endif
 
   
   file->Close();
