c26926c93660b2e955990653993895141a35cd1f
wuhongyi <wuhongyi@qq.com>
2021-09-01 22:19:01 +0800

    20210901

--- source/setup.sh
+++ source/setup.sh
@@ -5,9 +5,9 @@
 # Author: Hongyi Wu(吴鸿毅)
 # Email: wuhongyi@qq.com 
 # Created: 一 12月  3 17:22:32 2018 (+0800)
-# Last-Updated: 一 5月 11 12:02:09 2020 (+0800)
+# Last-Updated: 三 9月  1 22:04:49 2021 (+0800)
 #           By: Hongyi Wu(吴鸿毅)
-#     Update #: 2
+#     Update #: 6
 # URL: http://wuhongyi.cn 
 
 if [ `whoami` = "root" ];then 
@@ -23,25 +23,25 @@ fi
 
 num=`cat /proc/cpuinfo | grep processor | wc -l`
 
-tar -xzvf CAENVMELib-2.50.tgz
-cd CAENVMELib-2.50/lib
+tar -xzvf CAENVMELib-3.2.0-build20210409.tgz
+cd CAENVMELib-3.2.0/lib
 sh install_x64
 cd ../../
 
-tar -xvzf CAENComm-1.2-build20140211.tgz
-cd CAENComm-1.2/lib
+tar -xvzf CAENComm-1.4.1-build20210305.tgz
+cd CAENComm-1.4.1/lib
 sh install_x64
 cd ../../
 
-tar -xvzf CAENUpgrader-1.6.6.tgz
-cd CAENUpgrader-1.6.6
+tar -xvzf CAENUpgrader-1.7.1-build20210402.tgz
+cd CAENUpgrader-1.7.1
 ./configure
 make -j$num
 make install
 cd ..
 
 
-rm -rf CAENVMELib-2.50  CAENComm-1.2 CAENUpgrader-1.6.6 
+rm -rf CAENVMELib-3.2.0  CAENComm-1.4.1 CAENUpgrader-1.7.1
 echo "Install all software done !"
 
 # 
