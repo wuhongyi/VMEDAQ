b4461464b3984ece66d3a19dcc6c4bec7fd3c9ef
wuhongyi <wuhongyi@qq.com>
2021-09-06 21:08:29 +0800

    update driver

--- TODOLIST.rst
+++ TODOLIST.rst
@@ -4,9 +4,9 @@
 .. Author: Hongyi Wu(吴鸿毅)
 .. Email: wuhongyi@qq.com 
 .. Created: 六 12月 28 21:05:15 2019 (+0800)
-.. Last-Updated: 六 12月 28 21:07:50 2019 (+0800)
+.. Last-Updated: 四 9月  2 10:26:02 2021 (+0800)
 ..           By: Hongyi Wu(吴鸿毅)
-..     Update #: 1
+..     Update #: 2
 .. URL: http://wuhongyi.cn 
 
 ##################################################
@@ -16,6 +16,29 @@ TODO
 - 测试新版本软件 CAENUpgrader-1.6.6.tgz
 - 测试 MADC 最新固件 0230
 - 测试 Vx718 最新固件 2.17
+
+
+
+支持ubuntu
+
+babirl/babicon/Makefile
+
+.. code-block:: bash
+		
+   LOADLIBES = ../lib/libbabirl.a -lreadline -lncurses -lexpat
+   LDFLAGS += 
+
+
+
+DAQConfig/bbcaenvme/babiescmdme.c
+
+.. code-block:: cpp
+		
+   typedef struct {
+     enum mode md;
+     const char mdchar[4];
+   }stchkmode;
+
 
   
 .. 
--- source/setup.sh
+++ source/setup.sh
@@ -5,9 +5,9 @@
 # Author: Hongyi Wu(吴鸿毅)
 # Email: wuhongyi@qq.com 
 # Created: 一 12月  3 17:22:32 2018 (+0800)
-# Last-Updated: 三 9月  1 22:04:49 2021 (+0800)
+# Last-Updated: 一 9月  6 21:01:49 2021 (+0800)
 #           By: Hongyi Wu(吴鸿毅)
-#     Update #: 6
+#     Update #: 7
 # URL: http://wuhongyi.cn 
 
 if [ `whoami` = "root" ];then 
@@ -23,13 +23,13 @@ fi
 
 num=`cat /proc/cpuinfo | grep processor | wc -l`
 
-tar -xzvf CAENVMELib-3.2.0-build20210409.tgz
-cd CAENVMELib-3.2.0/lib
+tar -xzvf CAENVMELib-3.3.0-build20210806.tgz
+cd CAENVMELib-3.3.0/lib
 sh install_x64
 cd ../../
 
-tar -xvzf CAENComm-1.4.1-build20210305.tgz
-cd CAENComm-1.4.1/lib
+tar -xvzf CAENComm-1.5.0-build20210804.tgz
+cd CAENComm-1.5.0/lib
 sh install_x64
 cd ../../
 
@@ -41,7 +41,7 @@ make install
 cd ..
 
 
-rm -rf CAENVMELib-3.2.0  CAENComm-1.4.1 CAENUpgrader-1.7.1
+rm -rf CAENVMELib-3.3.0  CAENComm-1.5.0 CAENUpgrader-1.7.1
 echo "Install all software done !"
 
 # 
